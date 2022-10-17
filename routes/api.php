b0cb5fe1cce3adba494fa4be5d00173171e5054e
hafizi hamid <mohamadhafizihamid@gmail.com>
2022-10-17 23:01:48 +0800

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +15,35 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::group(
+    ['namespace' => 'Api', 'as' => 'api.', 'prefix' => 'v1'],
+    function () {
+        Route::get('/login', [AuthController::class, 'login']);
+
+        // Route::post('/login', 'CustomerAuthController@login')->name('login');
+        // Route::post('/register', 'CustomerAuthController@register')->name('register');
+
+        // ------- ADMIN API ----------
+        // Route::group(
+        //     ['prefix' => 'admin', 'as' => 'admin.'],
+        //     function () {
+        //         Route::post('/login', 'AuthController@login')->name('login');
+        //         Route::group(
+        //             ['prefix' => 'password', 'as' => 'password.'],
+        //             function () {
+        //                 Route::post('/forgot', 'AuthController@forgot')->name('forgot');
+        //                 Route::post('/reset', 'AuthController@reset')->name('reset');
+        //                 Route::post('/set', 'UserController@setPassword')->name('set');
+        //             }
+        //         );
+        //         Route::group(
+        //             ['middleware' => ['auth:api', 'scope:admin']],
+        //             function () {
+        //                 Route::get('/authcheck', 'AuthController@authCheck')->name('auth-check');
+        //                 Route::get('/logout', 'AuthController@logout')->name('logout');
+        //             }
+        //         );
+        //     }
+        // );
+    }
+);
